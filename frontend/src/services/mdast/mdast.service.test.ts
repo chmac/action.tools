157cac04913b4641b33ecead2fb336784178d6f5
Callum Macdonald <git@callum-macdonald.com>
2020-02-05 00:24:51 +0100

    More experiments. Some testing.

---
+++ frontend/src/services/mdast/mdast.service.test.ts
@@ -0,0 +1,51 @@
+import unified from "unified";
+import markdown from "remark-parse";
+
+// import { describe } from 'jest'
+import {
+  getTaskTitle,
+  markdownToMdast,
+  getAllTasksFromTree
+} from "./mdast.service";
+
+export const test = () => {};
+
+const example = `
+* [ ] Here's a top level task in a +Random project @home
+* [ ] With another second task +ClearOut @work
+  - A comment added to the second task
+  - [ ] A sub task is also underneath the comment
+`;
+
+describe("todo.md", () => {
+  describe("markdownToMdast #CSbVZJ", () => {
+    it("matches snapshot", () => {
+      const tree = markdownToMdast(example);
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  describe("getAllTasksFromTree()", () => {
+    it("Does not return the comment #oRaIwP", () => {
+      const tree = markdownToMdast(example);
+      const tasks = getAllTasksFromTree(tree);
+      expect(tasks).toHaveLength(3);
+    });
+  });
+
+  describe("getTaskTitle()", () => {
+    it("Gets title with project and context #pMaXbr", () => {
+      const tree = markdownToMdast(example);
+      const [one, two, three] = getAllTasksFromTree(tree);
+      expect(getTaskTitle(one)).toEqual(
+        "Here's a top level task in a +Random project @home"
+      );
+      expect(getTaskTitle(two)).toEqual(
+        "With another second task +ClearOut @work"
+      );
+      expect(getTaskTitle(three)).toEqual(
+        "A sub task is also underneath the comment"
+      );
+    });
+  });
+});
--- frontend/src/services/mdast/mdast.service.ts
+++ frontend/src/services/mdast/mdast.service.ts
@@ -6,21 +6,27 @@ import map from "unist-util-map";
 import { selectAll } from "unist-util-select";
 import { Node } from "unist";
 
-const isTask = (node: Node) => {
-  return typeof R.pipe(node, R.prop("checked")) === "boolean";
+export const markdownToMdast = (text: string) => {
+  return unified()
+    .use(markdown, { gfm: true })
+    .parse(text);
 };
 
-const getAllTasksFromTree = (tree: Node) => {
+export const isTask = (node: Node) => {
+  return node.type === "listItem" && typeof node.checked === "boolean";
+};
+
+export const getAllTasksFromTree = (tree: Node) => {
   const listItems = selectAll("listItem", tree);
   return R.filter(listItems, isTask);
 };
 
-const getTopLevelTasksFromTree = (tree: Node) => {
+export const getTopLevelTasksFromTree = (tree: Node) => {
   const listItems = selectAll(":root > list > listItem", tree);
   return R.filter(listItems, isTask);
 };
 
-const getTaskTitle = (task: Node) => {
+export const getTaskTitle = (task: Node) => {
   return R.map(
     selectAll(":root > paragraph > text", task),
     R.prop("value")
@@ -37,16 +43,27 @@ const addTitles = addField("title", getTaskTitle);
 
 const ProjectRegex = new RegExp("@[\\w]+", "g");
 
-const getProjectsFromText = (texts: string[]): string[] => {
-  return R.flatMap(texts, text => {
+export const getProjectsFromText = (texts: string[]): string[] => {
+  const matches = R.flatMap(texts, text => {
     return text.match(ProjectRegex) || [];
   });
+  return R.map(matches, match => match.substr(1));
 };
 
 const addProjects = addField("projects", (task: Node): string[] => {
   const matches = getProjectsFromText([(task as any).title]);
   return R.map(matches, match => match.substr(1));
 });
+
+export const buildDataForTask = (task: Node) => {
+  const title = getTaskTitle(task);
+  const projects = getProjectsFromText([title]);
+  return { title, projects };
+};
+
+export const addData = (task: Node) => {
+  return R.addProp(task, "_data", buildDataForTask(task));
+};
 
 export const startup = () => {
   fetch("/do.md")
@@ -59,17 +76,18 @@ export const startup = () => {
         .parse(body);
     })
     .then(tree => {
-      const allTasks = R.pipe(
-        getAllTasksFromTree(tree),
-        addTitles,
-        addProjects
-      );
+      return map(tree, (node: Node) => {
+        if (isTask(node)) {
+          return addData(node);
+        }
+        return node;
+      });
+    })
+    .then(treeWithDAta => {
+      debugger;
+      // const allTasks = R.pipe(getAllTasksFromTree(tree), R.map(addData));
 
-      const topTasks = R.pipe(
-        getTopLevelTasksFromTree(tree),
-        addTitles,
-        addProjects
-      );
+      // const topTasks = R.pipe(getTopLevelTasksFromTree(tree), R.map(addData));
 
       debugger;
     });
