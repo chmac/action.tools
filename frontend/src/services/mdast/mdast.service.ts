341e4e777c4d83dd8f3865a8e76a7d52499ac998
Callum Macdonald <git@callum-macdonald.com>
2020-02-05 20:03:19 +0100

    Move utils to utils file.

--- frontend/src/services/mdast/mdast.service.ts
+++ frontend/src/services/mdast/mdast.service.ts
@@ -6,16 +6,12 @@ import visit from "unist-util-visit";
 import map from "unist-util-map";
 import { selectAll } from "unist-util-select";
 import { Node } from "unist";
-import later from "later";
-import dayjs from "dayjs";
-import { Rule } from "../../rschedule";
-import { RuleOption } from "@rschedule/core";
 
-const EVERY = "every";
-const AFTER = "after";
-const UNITS = ["month", "day", "week"];
-const DAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-const MONTHS = [
+export const EVERY = "every";
+export const AFTER = "after";
+export const UNITS = ["month", "day", "week"];
+export const DAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+export const MONTHS = [
   "jan",
   "feb",
   "mar",
@@ -29,7 +25,7 @@ const MONTHS = [
   "nov",
   "dec"
 ];
-const MONTHS_TO_NUMBER: { [month: string]: number } = {
+export const MONTHS_TO_NUMBER: { [month: string]: number } = {
   jan: 1,
   feb: 2,
   mar: 3,
@@ -127,7 +123,6 @@ export const startup = () => {
       });
     })
     .then(treeWithDAta => {
-      const foo = later.parse.text("every monday,tuesday");
       debugger;
       // const allTasks = R.pipe(getAllTasksFromTree(tree), R.map(addData));
 
@@ -162,144 +157,4 @@ const treeToActions = (tree: any) => {
       debugger;
     });
   });
-};
-
-export const getNumberFromStart = (input: string): number => {
-  const result = input.match(/^[\d]+/);
-  if (!result || !result.length || !result[0]) {
-    throw new Error("Failed to find number #1jMxvU");
-  }
-  const number = parseInt(result[0]);
-  if (typeof number !== "number" || number <= 0) {
-    throw new Error("Invalid number found #GPU80T");
-  }
-  return number;
-};
-
-export const isValidUnit = (unit: string): unit is dayjs.OpUnitType => {
-  return UNITS.indexOf(unit) !== -1;
-};
-
-const convertUnit = (unit: string) => {
-  return unit.toLowerCase().replace(/s$/, "");
-};
-
-export const getCountAndUnitFromString = (
-  input: string
-): { count: number; unit: dayjs.OpUnitType } => {
-  const result = input.match(/^([\d]+)([\w]+)$/);
-
-  if (
-    !result ||
-    typeof result[1] !== "string" ||
-    typeof result[2] !== "string"
-  ) {
-    throw new Error("Invalid recurrence string #LK6o80");
-  }
-
-  const [_, countString, unitResult] = result;
-
-  const count = parseInt(countString);
-
-  if (count <= 0) {
-    throw new Error("Invalid count #E8DCQc");
-  }
-
-  const unit = convertUnit(unitResult);
-
-  if (!isValidUnit(unit)) {
-    throw new Error("Invalid unit specified #Fvk3ZI");
-  }
-
-  return {
-    count,
-    unit
-  };
-};
-
-export const startsWithANumber = (input: string): boolean => {
-  return !!input.match(/^\d/);
-};
-
-export const isDay = (input: string): boolean => {
-  return DAYS.indexOf(input) !== -1;
-};
-
-export const isMonth = (input: string): boolean => {
-  return MONTHS.indexOf(input) !== -1;
-};
-export const isValidPoint = (point: string) => {};
-
-export const startsWith = (target: string, input: string): boolean => {
-  return input.substr(0, target.length).toLowerCase() === target;
-};
-
-export const removeFromFront = (target: string, input: string): string => {
-  if (!startsWith(target, input)) {
-    return input;
-  }
-  return input.substr(target.length);
-};
-
-export const dayToRuleDay = (day: string): RuleOption.ByDayOfWeek => {
-  if (!isDay(day)) {
-    throw new Error("dayToRuleDay called with invalid day #TesvaG");
-  }
-  return day.substr(0, 2).toUpperCase() as RuleOption.ByDayOfWeek;
-};
-
-export const monthToRuleMonth = (month: string): RuleOption.ByMonthOfYear => {
-  if (!isMonth(month)) {
-    throw new Error("monthToRuleMonth called with invalid month #6rFg6u");
-  }
-  const lowerMonth = month.toLowerCase();
-  return MONTHS_TO_NUMBER[lowerMonth] as RuleOption.ByMonthOfYear;
-};
-
-export const calculateNextOccurrence = (schedule: string, from: Date): Date => {
-  if (startsWith(EVERY, schedule)) {
-    const extendBy = removeFromFront(EVERY, schedule);
-    if (startsWithANumber(extendBy)) {
-      const { count, unit } = getCountAndUnitFromString(extendBy);
-      return dayjs(from)
-        .add(count, unit)
-        .toDate();
-    }
-
-    const points = extendBy.split(",");
-
-    const arePointsAllDays = points.every(isDay);
-    const arePointsAllMonths = points.every(isMonth);
-    if (!(arePointsAllDays || arePointsAllMonths)) {
-      throw new Error("Invalid mixture of days and months #V0yUbX");
-    }
-
-    if (arePointsAllDays) {
-      const rule = new Rule({
-        frequency: "WEEKLY",
-        byDayOfWeek: R.map(points, dayToRuleDay),
-        start: from
-      });
-      const [next] = rule.occurrences({ start: from, take: 1 }).toArray();
-
-      return new Date(next.toISOString());
-    } else if (arePointsAllMonths) {
-      const rule = new Rule({
-        frequency: "YEARLY",
-        byMonthOfYear: R.map(points, monthToRuleMonth),
-        start: from
-      });
-
-      const [next] = rule.occurrences({ start: from, take: 1 }).toArray();
-
-      return new Date(next.toISOString());
-    }
-  } else if (startsWith(AFTER, schedule)) {
-    const extendBy = removeFromFront(AFTER, schedule);
-    const { count, unit } = getCountAndUnitFromString(extendBy);
-    return dayjs(from)
-      .add(count, unit)
-      .toDate();
-  }
-  throw new Error("Invalid repetition schedule #222CJy");
 };
