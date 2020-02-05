dfe63d0eff71bb7cde5315809ee4b8f8b31b042b
Callum Macdonald <git@callum-macdonald.com>
2020-02-05 17:10:11 +0100

    Calculate recurrence.

--- frontend/src/services/mdast/mdast.service.test.ts
+++ frontend/src/services/mdast/mdast.service.test.ts
@@ -1,11 +1,15 @@
-import unified from "unified";
-import markdown from "remark-parse";
+import MockDate from "mockdate";
 
 // import { describe } from 'jest'
 import {
   getTaskTitle,
   markdownToMdast,
-  getAllTasksFromTree
+  getAllTasksFromTree,
+  getNumberFromStart,
+  getCountAndUnitFromString,
+  isDay,
+  isMonth,
+  calculateNextOccurrence
 } from "./mdast.service";
 
 export const test = () => {};
@@ -18,6 +22,120 @@ const example = `
 `;
 
 describe("todo.md", () => {
+  describe("getNumberFromStart()", () => {
+    it("correctly grabs number from 34foo", () => {
+      expect(getNumberFromStart("34foo")).toEqual(34);
+    });
+
+    it("throws if the string does not start with a number", () => {
+      expect(() => {
+        getNumberFromStart("foo33");
+      }).toThrow();
+    });
+  });
+
+  describe("getNumberAndUnitFromString()", () => {
+    it("correctly extracts from 3day #sqJ6JF", () => {
+      expect(getCountAndUnitFromString("3day")).toEqual({
+        count: 3,
+        unit: "day"
+      });
+    });
+
+    it("correctly extracts from 3days (plural) #WSAMh5", () => {
+      expect(getCountAndUnitFromString("3days")).toEqual({
+        count: 3,
+        unit: "day"
+      });
+    });
+
+    it("correctly parses double digit units #agX0SB", () => {
+      expect(getCountAndUnitFromString("21weeks")).toEqual({
+        count: 21,
+        unit: "week"
+      });
+    });
+
+    it("throws with an invalid unit #ItO0lI", () => {
+      expect(() => getCountAndUnitFromString("33foo")).toThrow();
+    });
+
+    it("throws without a leading number #4g8Zd9", () => {
+      expect(() => getCountAndUnitFromString("foo33")).toThrow();
+    });
+
+    it("throws without a number #MM9D2H", () => {
+      expect(() => getCountAndUnitFromString("foobar")).toThrow();
+    });
+  });
+
+  describe("isDay()", () => {
+    it("accepts mon", () => {
+      expect(isDay("mon")).toEqual(true);
+    });
+
+    it("does not accept foo", () => {
+      expect(isDay("foo")).toEqual(false);
+    });
+
+    it("does not accept jan", () => {
+      expect(isDay("jan")).toEqual(false);
+    });
+  });
+
+  describe("isMonth()", () => {
+    it("accepts jan #lSTvXr", () => {
+      expect(isMonth("jan")).toEqual(true);
+    });
+
+    it("accepts dec #LMLuJM", () => {
+      expect(isMonth("dec")).toEqual(true);
+    });
+
+    it("does not accept mon #68KwVx", () => {
+      expect(isMonth("mon")).toEqual(false);
+    });
+  });
+
+  describe("calculateNextOccurrence()", () => {
+    const startTime = 1580917054000; // "2020-02-05T15:37:34.000";
+    const plusThreeDays = 1581176254000; // "2020-02-08T15:37:34.000";
+    const plusThreeWeeks = 1582731454000; // "2020-02-26T15:37:34.000";
+    // This next date has jumped over a DST change
+    const plusThreeMonths = 1588689454000; // "2020-05-05T14:37:34.000";
+
+    beforeAll(() => {
+      MockDate.set(startTime);
+    });
+    afterAll(() => {
+      MockDate.reset();
+    });
+
+    it("does not throw #jjJjDz", () => {
+      expect(() =>
+        calculateNextOccurrence("every2days", new Date(startTime))
+      ).not.toThrow();
+    });
+
+    it("adds 3 days #6gThb9", () => {
+      expect(
+        calculateNextOccurrence("every3days", new Date(startTime))
+      ).toEqual(new Date(plusThreeDays));
+    });
+
+    it("adds 3 weeks #KW3nYS", () => {
+      expect(
+        calculateNextOccurrence("every3weeks", new Date(startTime))
+      ).toEqual(new Date(plusThreeWeeks));
+    });
+
+    it("adds 3 months #1UX6WC", () => {
+      expect(
+        calculateNextOccurrence("every3months", new Date(startTime))
+      ).toEqual(new Date(plusThreeMonths));
+    });
+  });
+
   describe("markdownToMdast #CSbVZJ", () => {
     it("matches snapshot", () => {
       const tree = markdownToMdast(example);
--- frontend/src/services/mdast/mdast.service.ts
+++ frontend/src/services/mdast/mdast.service.ts
@@ -5,6 +5,27 @@ import visit from "unist-util-visit";
 import map from "unist-util-map";
 import { selectAll } from "unist-util-select";
 import { Node } from "unist";
+import later from "later";
+import dayjs from "dayjs";
+
+const EVERY = "every";
+const AFTER = "after";
+const UNITS = ["month", "day", "week"];
+const DAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const MONTHS = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec"
+];
 
 export const markdownToMdast = (text: string) => {
   return unified()
@@ -84,6 +105,7 @@ export const startup = () => {
       });
     })
     .then(treeWithDAta => {
+      const foo = later.parse.text("every monday,tuesday");
       debugger;
       // const allTasks = R.pipe(getAllTasksFromTree(tree), R.map(addData));
 
@@ -118,4 +140,103 @@ const treeToActions = (tree: any) => {
       debugger;
     });
   });
+};
+
+export const getNumberFromStart = (input: string): number => {
+  const result = input.match(/^[\d]+/);
+  if (!result || !result.length || !result[0]) {
+    throw new Error("Failed to find number #1jMxvU");
+  }
+  const number = parseInt(result[0]);
+  if (typeof number !== "number" || number <= 0) {
+    throw new Error("Invalid number found #GPU80T");
+  }
+  return number;
+};
+
+export const isValidUnit = (unit: string): unit is dayjs.OpUnitType => {
+  return UNITS.indexOf(unit) !== -1;
+};
+
+const convertUnit = (unit: string) => {
+  return unit.toLowerCase().replace(/s$/, "");
+};
+
+export const getCountAndUnitFromString = (
+  input: string
+): { count: number; unit: dayjs.OpUnitType } => {
+  const result = input.match(/^([\d]+)([\w]+)$/);
+
+  if (
+    !result ||
+    typeof result[1] !== "string" ||
+    typeof result[2] !== "string"
+  ) {
+    throw new Error("Invalid recurrence string #LK6o80");
+  }
+
+  const [_, countString, unitResult] = result;
+
+  const count = parseInt(countString);
+
+  if (count <= 0) {
+    throw new Error("Invalid count #E8DCQc");
+  }
+
+  const unit = convertUnit(unitResult);
+
+  if (!isValidUnit(unit)) {
+    throw new Error("Invalid unit specified #Fvk3ZI");
+  }
+
+  return {
+    count,
+    unit
+  };
+};
+
+export const startsWithANumber = (input: string): boolean => {
+  return !!input.match(/^\d/);
+};
+
+export const isDay = (input: string): boolean => {
+  return DAYS.indexOf(input) !== -1;
+};
+
+export const isMonth = (input: string): boolean => {
+  return MONTHS.indexOf(input) !== -1;
+};
+export const isValidPoint = (point: string) => {};
+
+export const calculateNextOccurrence = (schedule: string, from: Date): Date => {
+  if (schedule.substr(0, EVERY.length) === EVERY) {
+    const extendBy = schedule.substr(EVERY.length);
+
+    if (startsWithANumber(extendBy)) {
+      const { count, unit } = getCountAndUnitFromString(extendBy);
+      return dayjs(from)
+        .add(count, unit)
+        .toDate();
+    }
+
+    const points = extendBy.split(",");
+    if (!points.every(isDay) || !points.every(isMonth)) {
+      throw new Error("Invalid mixture of days and months #V0yUbX");
+    }
+
+    const compiledSchedules = later.parse.text(`on ${points.join(", ")}`);
+    const [next] = later
+      .schedule({
+        schedules: compiledSchedules
+      })
+      .next(1, from);
+    return next;
+  } else if (schedule.substr(0, AFTER.length) === AFTER) {
+    const extendBy = schedule.substr(AFTER.length);
+    const { count, unit } = getCountAndUnitFromString(extendBy);
+    return dayjs(from)
+      .add(count, unit)
+      .toDate();
+  }
+  throw new Error("Invalid repetition schedule #222CJy");
 };
