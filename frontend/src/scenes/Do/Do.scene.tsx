6867ae907ba83b95d95cd93e011ffe60d656a832
Callum Macdonald <git@callum-macdonald.com>
2020-02-25 22:25:29 +0100

    Ugly checking / unchecking. Works.
    Props https://github.com/rexxars/react-markdown/issues/298

---
+++ frontend/src/scenes/Do/Do.scene.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import { listItem as defaultListItem } from "react-markdown/lib/renderers";
+import Typography from "@material-ui/core/Typography";
+
+const markdownChecked = "- [x]";
+const markdownUnchecked = "- [ ]";
+
+const WrapCheckBox = (props: any) => {
+  const { markdown, setMarkdown, sourcePosition, checked, children } = props;
+  return (
+    <div
+      onClick={() => {
+        // TODO - Flip the checkbox here
+        // sourcePosition.start.line, sourcePosition.start.column, sourcePosition.start.offset
+        // sourcePosition.end.line, sourcePosition.end.column, sourcePosition.end.offset
+        debugger;
+        const lineIndex = sourcePosition.start.line - 1;
+        const lines = markdown.split("\n");
+        const find = checked ? markdownChecked : markdownUnchecked;
+        const replace = checked ? markdownUnchecked : markdownChecked;
+        lines[lineIndex] = lines[lineIndex].replace(find, replace);
+        setMarkdown(lines.join("\n"));
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Do = () => {
+  const [markdown, setMarkdown] = useState("");
+  useEffect(() => {
+    fetch("/do.md")
+      .then(response => response.text())
+      .then(markdown => setMarkdown(markdown));
+  }, []);
+
+  const renderers = {
+    listItem: (props: any) => {
+      if (typeof props.checked === "boolean") {
+        const { checked, sourcePosition } = props;
+        // debugger;
+        return (
+          <WrapCheckBox
+            markdown={markdown}
+            setMarkdown={setMarkdown}
+            checked={checked}
+            sourcePosition={sourcePosition}
+          >
+            {defaultListItem(props)}
+          </WrapCheckBox>
+        );
+      }
+      return defaultListItem(props);
+    }
+  };
+
+  return (
+    <div>
+      <Typography variant="h1">Do</Typography>
+      <ReactMarkdown source={markdown} renderers={renderers} rawSourcePos />
+    </div>
+  );
+};
+
+export default Do;
--- frontend/src/scenes/Routes/Routes.scene.tsx
+++ frontend/src/scenes/Routes/Routes.scene.tsx
@@ -10,6 +10,7 @@ import { ThemeProvider } from "@material-ui/styles";
 import Bar from "../Bar/Bar.scene";
 import Home from "../Home/Home.scene";
 import Actions from "../Actions/Actions.scene";
+import Do from "../Do/Do.scene";
 
 const baseTheme = createMuiTheme();
 const theme = responsiveFontSizes(baseTheme);
@@ -27,7 +28,8 @@ const Routes: React.FC = () => {
         <Container className={classes.container}>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/do" component={Actions} />
+            <Route exact path="/actions" component={Actions} />
+            <Route exact path="/do" component={Do} />
           </Switch>
         </Container>
       </Router>
