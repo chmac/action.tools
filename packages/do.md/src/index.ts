5c1bcba21d09434f0ef5b425d3cc2548e020f901
Callum Macdonald <git@callum-macdonald.com>
2020-02-28 15:34:44 +0100

    Export `today()`.

--- packages/do.md/src/index.ts
+++ packages/do.md/src/index.ts
@@ -1,4 +1,8 @@
-export { LocalDate } from "@js-joda/core";
+import { LocalDate } from "@js-joda/core";
 
 export { repeatTasks } from "./repeat";
 export { filterTasks } from "./filter";
+
+export const today = (): LocalDate => {
+  return LocalDate.now();
+};
