ae1146eb046e5bfcfed467a67cc9a1de97a100a6
Callum Macdonald <git@callum-macdonald.com>
2020-03-30 19:34:53 +0200

    Show task count.

--- frontend/src/scenes/Do/components/Markdown.component.tsx
+++ frontend/src/scenes/Do/components/Markdown.component.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from "react";
 import unified from "unified";
 import remark2rehype from "remark-rehype";
 import rehype2react from "rehype-react";
-import { filterTasks, today } from "do.md";
+import { filterTasks, today, countTasks } from "do.md";
 import { Node, Parent } from "unist";
 import listItemDefault from "mdast-util-to-hast/lib/handlers/list-item";
 import { isTask } from "do.md/dist/utils";
@@ -11,6 +11,7 @@ import { markdownToMdast } from "../../../services/mdast/mdast.service";
 import TaskFactory, { SetCheckedByLineNumber } from "./Task.component";
 import DataFactory from "./Data.component";
 import { Filter } from "do.md/dist/filter";
+import { Typography } from "@material-ui/core";
 
 type H = (node: any, tagName: string, props: {}, children: Node[]) => Node;
 
@@ -46,6 +47,8 @@ const Markdown = (props: Props) => {
     // Then apply our filter settings
     const filtered = filterTasks(mdast, filter);
 
+    const count = countTasks(filtered);
+
     // Now we convert the mdast into an hast
     const hast = toRehypeProcessor.runSync(filtered);
 
@@ -60,10 +63,17 @@ const Markdown = (props: Props) => {
       })
       .stringify(hast);
 
-    return elements;
+    return { count, elements };
   }, [markdown, filter, setCheckedByLineNumber]);
 
-  return <div>{getReact()}</div>;
+  const { count, elements } = getReact();
+
+  return (
+    <>
+      <Typography>Count: {count}</Typography>
+      <div>{elements}</div>
+    </>
+  );
 };
 
 export default Markdown;
