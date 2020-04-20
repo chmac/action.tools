6b32dcf58a765dedc11e6044cdf7863cd690cbe0
Callum Macdonald <git@callum-macdonald.com>
2020-04-20 17:13:41 +0200

    Fixture for empty headings.

--- packages/do.md/src/__tests__/__fixtures__/tasks.fixtures.ts
+++ packages/do.md/src/__tests__/__fixtures__/tasks.fixtures.ts
@@ -122,3 +122,76 @@ export const tree = u("root", [
     ]),
   ]),
 ]);
+
+// A tree of tasks including some headings which can be hidden because they
+// contain no tasks
+export const treeWithEmptyHeadings = u("root", [
+  u("list", { ordered: false, spread: false, start: null }, [
+    u("listeItem", { checked: false, spread: false }, [
+      u("paragraph", [
+        u("text", { value: "This is a task which is unchecked #Gf6a8R" }),
+      ]),
+    ]),
+    u("listeItem", { checked: false, spread: false }, [
+      u("paragraph", [
+        u("text", { value: "This is another task also unchecked #IcL6qn" }),
+      ]),
+    ]),
+    u("listeItem", { checked: null, spread: false }, [
+      u("paragraph", [
+        u("text", { value: "This is another task also unchecked #IcL6qn" }),
+      ]),
+    ]),
+  ]),
+  u("heading", { depth: 1 }, [u("text", { value: "First Heading" })]),
+  u("heading", { depth: 2 }, [u("text", { value: "Second Level 2 Heading" })]),
+  u("paragraph", [
+    u("text", {
+      value: "This is a paragraph which contains a whole chunk of text",
+    }),
+  ]),
+  u("paragraph", [
+    u("text", {
+      value: "Followed by a paragraph of text",
+    }),
+  ]),
+  u("list", { ordered: false, spread: false, start: null }, [
+    // Top level task
+    u("listeItem", { checked: false, spread: false }, [
+      u("paragraph", [
+        u("text", { value: "This is a top level task #xAgxB9" }),
+      ]),
+      // Nested list
+      u("list", { ordered: false, spread: false, start: null }, [
+        u("listeItem", { checked: false, spread: false }, [
+          u("paragraph", [
+            u("text", { value: "This is a child task #JkmkMh" }),
+          ]),
+        ]),
+        u("listeItem", { checked: false, spread: false }, [
+          u("paragraph", [u("text", { value: "Second child task #PWfUG1" })]),
+        ]),
+        u("listeItem", { checked: null, spread: false }, [
+          u("paragraph", [
+            u("text", { value: "Third child level task #eHaqOJ" }),
+          ]),
+        ]),
+      ]),
+    ]),
+    u("listeItem", { checked: true, spread: false }, [
+      u("paragraph", [
+        u("text", { value: "Back to a top level completed task #FVK0TU" }),
+      ]),
+    ]),
+    u("listeItem", { checked: null, spread: false }, [
+      u("paragraph", [u("text", { value: "Final top level task #lJNUwz" })]),
+    ]),
+  ]),
+  u("heading", { depth: 1 }, [u("text", { value: "Empty Top Heading" })]),
+  u("heading", { depth: 2 }, [u("text", { value: "Empty sub h2" })]),
+  u("paragraph", [
+    u("text", {
+      value: "This is a paragraph without any task children.",
+    }),
+  ]),
+]);
