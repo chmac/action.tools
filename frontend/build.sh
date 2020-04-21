4cf9585eb011a4c9ab94d6d9b3a399ed1a52be1c
Callum Macdonald <git@callum-macdonald.com>
2020-04-21 16:10:44 +0200

    Add git status to build script.

--- frontend/build.sh
+++ frontend/build.sh
@@ -9,3 +9,5 @@ react-scripts build
 echo writing version file
 echo $REACT_APP_VERSION_GIT > build/version.txt
 date +%Y-%m-%d_%H-%M-%S >> build/version.txt
+echo >> build/version.txt
+git status --short >> build/version.txt
