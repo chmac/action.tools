e6db0808e3291b2a8d6a9176284f812316b4d830
Callum Macdonald <git@callum-macdonald.com>
2020-04-21 15:42:23 +0200

    Export trim.

--- packages/do.md/src/index.ts
+++ packages/do.md/src/index.ts
@@ -3,6 +3,7 @@ import { LocalDate } from "@js-joda/core";
 export { repeatTasks } from "./repeat";
 export { filterTasks } from "./filter";
 export { countTasks } from "./count";
+export { trim } from "./trim";
 
 export const today = (): LocalDate => {
   return LocalDate.now();
