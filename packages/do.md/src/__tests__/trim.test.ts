d19b4c3247a904fe15de476952964a1a8311bbbc
Callum Macdonald <git@callum-macdonald.com>
2020-04-21 16:17:56 +0200

    Add failing test.

--- packages/do.md/src/__tests__/trim.test.ts
+++ packages/do.md/src/__tests__/trim.test.ts
@@ -16,5 +16,31 @@ describe("trim", () => {
       const input = R.clone(treeWithEmptyHeadings);
       expect(trim(input)).toEqual(treeWithEmptyHeadingsTrimmed);
     });
+
+    it("Trims out sibling headings without any sub tasks #Sqv5fV", () => {
+      const input = u("root", [
+        u("heading", { depth: 1 }, [u("text", "First heading")]),
+        u("heading", { depth: 2 }, [u("text", "Empty h2")]),
+        u("paragraph", [
+          u("text", "Para below heading without a task should be stripped"),
+        ]),
+        u("heading", { depth: 2 }, [u("text", "Second repeat heading")]),
+        u("list", { ordered: false, spread: false, start: null }, [
+          u("listItem", { checked: false, spread: false }, [
+            u("paragraph", [u("text", "This is a child task #JkmkMh")]),
+          ]),
+        ]),
+      ]);
+      const expected = u("root", [
+        u("heading", { depth: 1 }, [u("text", "First heading")]),
+        u("heading", { depth: 2 }, [u("text", "Second repeat heading")]),
+        u("list", { ordered: false, spread: false, start: null }, [
+          u("listItem", { checked: false, spread: false }, [
+            u("paragraph", [u("text", "This is a child task #JkmkMh")]),
+          ]),
+        ]),
+      ]);
+      expect(trim(input)).toEqual(expected);
+    });
   });
 });
