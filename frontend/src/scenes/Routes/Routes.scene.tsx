6855f39a6b5776461c6f3ae7a4b0bfaf32022aa1
Callum Macdonald <git@callum-macdonald.com>
2020-06-15 16:46:28 +0200

    Trigger startup from routes.

--- frontend/src/scenes/Routes/Routes.scene.tsx
+++ frontend/src/scenes/Routes/Routes.scene.tsx
@@ -3,10 +3,17 @@ import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { makeStyles, responsiveFontSizes } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
+import { startup as domdStartup } from "do.md";
 import { createBrowserHistory } from "history";
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Route, Router, Switch } from "react-router-dom";
 import { SnackbarContainer } from "uno-material-ui";
+import {
+  getMarkdown,
+  startup as storageStartup,
+} from "../../services/storage/storage.service";
+import { AppDispatch } from "../../store";
 import Actions from "../Actions/Actions.scene";
 import Bar from "../Bar/Bar.scene";
 import Do from "../Do/Do.scene";
@@ -19,6 +26,14 @@ export const history = createBrowserHistory();
 
 const Routes: React.FC = () => {
   const classes = useStyles();
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    storageStartup().then(async () => {
+      const markdown = await getMarkdown();
+      dispatch(domdStartup({ markdown }));
+    });
+  }, [dispatch]);
 
   return (
     <ThemeProvider theme={theme}>
@@ -41,8 +56,8 @@ const Routes: React.FC = () => {
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      flexGrow: 1
-    }
+      flexGrow: 1,
+    },
   })
 );
 
