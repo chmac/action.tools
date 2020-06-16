b8ca5d534c399b1b2f802e0faddb414118477c31
Callum Macdonald <git@callum-macdonald.com>
2020-06-16 13:58:58 +0200

    Add util packages.

--- packages/do.md/src/modules.d.ts
+++ packages/do.md/src/modules.d.ts
@@ -1,3 +1,9 @@
+declare module 'unist-util-remove-position' {
+  import { Node } from 'unist';
+  const removePosition: <T extends Node>(node: T, force?: boolean) => T;
+  export default removePosition;
+}
+
 declare module 'mdast-util-to-string' {
   import { Node } from 'unist';
   const toString: (input: Node) => string;
