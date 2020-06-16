2bf0cd48feecedb37b762d12924ff26583f3c436
Callum Macdonald <git@callum-macdonald.com>
2020-06-16 21:24:53 +0200

    Set markdown again on startup.

--- frontend/src/startup.ts
+++ frontend/src/startup.ts
@@ -7,13 +7,43 @@ import {
 import store, { AppState } from "./store";
 import { mdastToMarkdown } from "./services/mdast/mdast.service";
 
+// We use this to try and ensure only 1 operation runs at a time
+let block = false;
+let lastMarkdown = "";
+
+const setMarkdownFromReduxState = async (state: AppState["__domd"]) => {
+  if (block === true) {
+    console.log("blocked, calling setTimeout #CRm6Yn");
+    setTimeout(() => setMarkdownFromReduxState(state), 30);
+    return;
+  }
+  block = true;
+  const { sections, tasks } = state.data;
+  const mdast = createMdast({ sections, tasks });
+  const markdown = await mdastToMarkdown(mdast);
+
+  if (markdown !== lastMarkdown) {
+    await setMarkdown(markdown);
+  } else {
+    console.log("setMarkdown() skipped for duplicate content #dCCYs2");
+  }
+
+  block = false;
+};
+
 const startup = async () => {
   await storageStartup();
   const markdown = await getMarkdown();
   await store.dispatch(domdStartup({ markdown }));
 
+  lastMarkdown = markdown;
+
+  // After successful startup, try writing the markdown immediately, to ensure
+  // that we separate any transforms from operations carried out by the user.
+  await setMarkdownFromReduxState(store.getState().__domd);
+
   let lastState: AppState["__domd"];
-  store.subscribe(async () => {
+  store.subscribe(() => {
     const state = store.getState();
 
     // Do nothing until the initial data load has finished
@@ -24,10 +54,7 @@ const startup = async () => {
 
     if (lastState !== newState) {
       lastState = newState;
-      const { sections, tasks } = newState.data;
-      const mdast = createMdast({ sections, tasks });
-      const markdown = await mdastToMarkdown(mdast);
-      setMarkdown(markdown);
+      setMarkdownFromReduxState(newState);
     }
   });
 };
