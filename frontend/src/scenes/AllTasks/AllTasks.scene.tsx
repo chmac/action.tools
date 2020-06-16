534919c00f2fae63e764e3bc49037f450bd53f3f
Callum Macdonald <git@callum-macdonald.com>
2020-06-16 21:31:38 +0200

    Linting cleanup.

--- frontend/src/scenes/AllTasks/AllTasks.scene.tsx
+++ frontend/src/scenes/AllTasks/AllTasks.scene.tsx
@@ -1,36 +1,9 @@
 import { makeStyles, Typography } from "@material-ui/core";
 import { REDUX_ROOT_KEY } from "do.md";
-import React, { createElement } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-import rehype2react from "rehype-react";
-import remark2react from "remark-react";
-import remark2rehype from "remark-rehype";
-import unified from "unified";
-import { Node } from "unist";
 import { AppState } from "../../store";
 import SectionScene from "./scenes/Section/Section.scene";
-
-const converterOld = unified()
-  .use(remark2rehype)
-  .use(rehype2react, { createElement });
-const processor = unified().use(remark2react, { createElement });
-
-const mdastToReact = <T extends Node>(content?: T | T[]) => {
-  if (typeof content === "undefined") {
-    return null;
-  }
-
-  const children = Array.isArray(content) ? content : [content];
-
-  if (children.length === 0) {
-    return null;
-  }
-
-  return processor.stringify({
-    type: "root",
-    children,
-  });
-};
 
 const AllTasks = () => {
   const classes = useStyles();
