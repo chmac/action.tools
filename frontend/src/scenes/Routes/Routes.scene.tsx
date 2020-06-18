9b1405831ac4e006b48a4b29bc39ff6fb72a5cbd
Callum Macdonald <git@callum-macdonald.com>
2020-06-18 13:50:28 +0200

    Create full screen loading indicator.
    Runs before task data has loaded.

--- frontend/src/scenes/Routes/Routes.scene.tsx
+++ frontend/src/scenes/Routes/Routes.scene.tsx
@@ -1,12 +1,20 @@
-import { createMuiTheme, createStyles, Theme } from "@material-ui/core";
+import {
+  CircularProgress,
+  createMuiTheme,
+  createStyles,
+  Theme,
+} from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { makeStyles, responsiveFontSizes } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
+import { getPackageState as getDomdState } from "do.md";
 import { createBrowserHistory } from "history";
 import React from "react";
+import { useSelector } from "react-redux";
 import { Route, Router, Switch } from "react-router-dom";
 import { SnackbarContainer } from "uno-material-ui";
+import { AppState } from "../../store";
 import Actions from "../Actions/Actions.scene";
 import AllTasks from "../AllTasks/AllTasks.scene";
 import Bar from "../Bar/Bar.scene";
@@ -19,8 +27,21 @@ const theme = responsiveFontSizes(baseTheme);
 
 export const history = createBrowserHistory();
 
+const Loading = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.loadingWrapper}>
+      <CircularProgress size="6rem" />
+    </div>
+  );
+};
+
 const Routes: React.FC = () => {
   const classes = useStyles();
+  const dataLoaded = useSelector(
+    (state: AppState) => getDomdState(state).data.initialDataLoadComplete
+  );
 
   return (
     <ThemeProvider theme={theme}>
@@ -29,13 +50,17 @@ const Routes: React.FC = () => {
         <Bar />
         <CssBaseline />
         <Container className={classes.container}>
-          <Switch>
-            <Route exact path="/" component={Do} />
-            <Route exact path="/tasks" component={AllTasks} />
-            <Route exact path="/review" component={Consecutive} />
-            <Route exact path="/actions" component={Actions} />
-            <Route exact path="/home" component={Home} />
-          </Switch>
+          {dataLoaded ? (
+            <Switch>
+              <Route exact path="/" component={Do} />
+              <Route exact path="/tasks" component={AllTasks} />
+              <Route exact path="/review" component={Consecutive} />
+              <Route exact path="/actions" component={Actions} />
+              <Route exact path="/home" component={Home} />
+            </Switch>
+          ) : (
+            <Loading />
+          )}
         </Container>
       </Router>
     </ThemeProvider>
@@ -46,6 +71,16 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
       flexGrow: 1,
+    },
+    loadingWrapper: {
+      width: "100vh",
+      height: "100vh",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
     },
   })
 );
