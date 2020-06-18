43546f584d23756e1a135d6efdad500c3dec1672
Callum Macdonald <git@callum-macdonald.com>
2020-06-18 14:09:37 +0200

    Add `hasChildren` to `taskById` selector result.

--- packages/do.md/src/services/query/selectors/taskById.selector.ts
+++ packages/do.md/src/services/query/selectors/taskById.selector.ts
@@ -1,5 +1,15 @@
 import { RootState, getLocalState } from '../../../store';
 
 export const taskById = (state: RootState, id: string) => {
-  return getLocalState(state).data.tasks.find(task => task.id === id);
+  const tasks = getLocalState(state).data.tasks;
+  const task = tasks.find(task => task.id === id);
+
+  if (typeof task === 'undefined') {
+    throw new Error('Tried to find non existent task. #fUNAfb');
+  }
+
+  const hasChildren =
+    typeof tasks.find(task => task.parentId === task.id) !== 'undefined';
+
+  return { ...task, hasChildren };
 };
