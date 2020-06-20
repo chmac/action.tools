c426078152aafb836917476ed9e21f3f24960ec4
Callum Macdonald <git@callum-macdonald.com>
2020-06-20 19:32:00 +0200

    Children must complete before parents.

--- packages/do.md/src/services/query/selectors/actionableToday.selector.ts
+++ packages/do.md/src/services/query/selectors/actionableToday.selector.ts
@@ -10,13 +10,27 @@ export const makeActionableTodaySelector = () => {
       (state: RootState) => getLocalState(state).data.tasks,
     ],
     (today, currentContexts, tasks) => {
-      return tasks.filter(task =>
-        isTaskActionableToday({
+      return tasks.filter(task => {
+        // If this task has actionable children, then consider it is not
+        // "actionable" today, the children must be completed first. This is a
+        // slightly weird dependency decision, and might need revisited, because
+        // after the children are completed, the parent becomes available.
+        // TODO Figure out how to handle child / parent tasks
+        const { id } = task;
+        if (
+          typeof tasks.find(
+            task => task.parentId === id && task.isTask && !task.finished
+          ) !== 'undefined'
+        ) {
+          return false;
+        }
+
+        return isTaskActionableToday({
           task,
           today,
           currentContexts,
-        })
-      );
+        });
+      });
     }
   );
 };
