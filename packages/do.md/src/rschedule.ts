376bddee8b4849b4f2760474a0e12b2b8cb3f93e
Callum Macdonald <git@callum-macdonald.com>
2020-06-25 22:36:00 +0200

    Fix export of Rule.

--- packages/do.md/src/rschedule.ts
+++ packages/do.md/src/rschedule.ts
@@ -8,4 +8,8 @@ import '@rschedule/dayjs-date-adapter/setup';
 
 export * from '@rschedule/dayjs-date-adapter';
 export * from '@rschedule/core';
+// NOTE: This is required to somehow fix the exports, otherwise we end up with
+// `import { Rule } from '@rschedule/dayjs-date-adapter'` in the built esm
+// module. Unclear why, this is a hack but seems to work. Rollup maybe.
+export { Rule } from '@rschedule/core/generators';
 export * from '@rschedule/core/generators';
