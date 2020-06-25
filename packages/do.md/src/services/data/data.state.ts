25b9aea435198899a9e8b52cc62b6b8595db5fa3
Callum Macdonald <git@callum-macdonald.com>
2020-06-25 23:41:42 +0200

    Make finishTask() into a thunk.
    Handle repetition on completion. Seems to work. Lightly tested.

--- packages/do.md/src/services/data/data.state.ts
+++ packages/do.md/src/services/data/data.state.ts
@@ -1,10 +1,15 @@
-import { createSlice, PayloadAction, createAction } from '@reduxjs/toolkit';
-import { Section, Task } from '../../types';
+import { createAction, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import dayjs from 'dayjs';
-import { stringifyDayjs } from '../../utils';
 import { difference } from 'remeda';
+import { REDUX_ROOT_KEY } from '../../constants';
+import { AppThunk, RootState } from '../../store';
+import { Section, Task } from '../../types';
+import { stringifyDayjs } from '../../utils';
+import { createNextIteration } from '../repeat/repeat.service';
 
 export const REDUX_KEY = 'data';
+
+const getState = (state: RootState) => state[REDUX_ROOT_KEY][REDUX_KEY];
 
 type State = {
   sections: Section[];
@@ -18,7 +23,7 @@ const initialState: State = {
 };
 
 const getTaskIndex = (state: State, id: string): number => {
-  const taskIndex = state.tasks.findIndex(task => task.id === id);
+  const taskIndex = state.tasks.findIndex((task) => task.id === id);
   if (typeof taskIndex === 'undefined') {
     throw new Error('Failed to find task by ID #1INCd9');
   }
@@ -26,7 +31,7 @@ const getTaskIndex = (state: State, id: string): number => {
 };
 
 export const snoozeTask = createAction(
-  'snoozeTask',
+  'domd/data/snoozeTask',
   ({ id, daysFromToday }: { id: string; daysFromToday: number }) => {
     return {
       payload: {
@@ -68,7 +73,7 @@ const sectionSlice = createSlice({
     ) => {
       state.tasks.splice(action.payload.insertAtIndex, 0, action.payload.task);
     },
-    finishTask: (state, action: PayloadAction<string>) => {
+    _finishTask: (state, action: PayloadAction<string>) => {
       const taskIndex = getTaskIndex(state, action.payload);
       state.tasks[taskIndex].finished = true;
     },
@@ -113,11 +118,19 @@ const sectionSlice = createSlice({
       task.data.contexts = task.data.contexts.concat(insertContexts);
     },
   },
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder.addCase(snoozeTask, (state, action) => {
       const taskIndex = getTaskIndex(state, action.payload.id);
       state.tasks[taskIndex].data.snooze = action.payload.snooze;
     });
+    // .addCase(finishTask, (state, action) => {
+    //   const taskIndex = getTaskIndex(state, action.payload.id);
+
+    //   // NOTE: This must happen BEFORE the `.splice()` call as it will
+    //   // change the index order of the array.
+    //   state.tasks[taskIndex].finished = true;
+    //   state.tasks.splice(taskIndex, 0, action.payload.id);
+    // });
   },
 });
 
@@ -125,9 +138,35 @@ export const {
   setData,
   newSection,
   newTask,
-  finishTask,
   unfinishTask,
   addContextsToTask,
 } = sectionSlice.actions;
 
 export default sectionSlice.reducer;
+
+export const finishTask = (id: string): AppThunk => async (
+  dispatch,
+  getRootState
+) => {
+  const rootState = getRootState();
+  const state = getState(rootState);
+
+  const taskIndex = getTaskIndex(state, id);
+  const task = state.tasks[taskIndex];
+
+  dispatch(sectionSlice.actions._finishTask(id));
+
+  // If this task has no repetition, then stop here
+  if (typeof task.data.repeat === 'undefined') {
+    return;
+  }
+
+  const today = rootState[REDUX_ROOT_KEY].query.today;
+
+  const nextIteration = createNextIteration({
+    task,
+    today,
+  });
+
+  dispatch(newTask({ task: nextIteration, insertAtIndex: taskIndex }));
+};
