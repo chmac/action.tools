f21facee87c409aa764e33c014164b2a17a1fff4
Callum Macdonald <git@callum-macdonald.com>
2020-07-01 12:49:22 +0200

    Update `selectTasks` to filter by `isTask`.

--- packages/do.md/src/services/query/selectors/selectTasks.selector.ts
+++ packages/do.md/src/services/query/selectors/selectTasks.selector.ts
@@ -1,4 +1,10 @@
-import { RootState, getLocalState } from '../../../store';
+import { getLocalState, RootState } from '../../../store';
+import { Task } from '../../../types';
 
-export const selectTasks = (rootState: RootState) =>
-  getLocalState(rootState).data.tasks;
+export const isTask = (task: Task) => task.isTask;
+
+export const selectTasks = (rootState: RootState) => {
+  const tasks = getLocalState(rootState).data.tasks;
+
+  return tasks.filter(isTask);
+};
