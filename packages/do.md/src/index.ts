ee47e798bd6bbcb5d1b6a486b9d04e921dcba767
Callum Macdonald <git@callum-macdonald.com>
2020-09-02 12:53:40 +0200

    Remove junk export.

--- packages/do.md/src/index.ts
+++ packages/do.md/src/index.ts
@@ -82,10 +82,3 @@ export { createId, stringifyDayjs } from './utils';
  *   - parentTaskId
  *   - depth
  */
-
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
