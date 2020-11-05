bb8efa363788d1aa9100d0e9b96cd71eb77d686d
Callum Macdonald <git@callum-macdonald.com>
2020-11-05 15:01:30 +0100

    Experiment in grouping tasks in review.
    Not sure how useful this is. It does make today's due tasks more visible, and splits dated / undated tasks. Will see...

--- frontend/src/scenes/Review/scenes/ReviewAll/ReviewAll.scene.tsx
+++ frontend/src/scenes/Review/scenes/ReviewAll/ReviewAll.scene.tsx
@@ -5,13 +5,20 @@ import {
   Switch,
   Typography,
 } from "@material-ui/core";
-import { selectActionableTodayFactory } from "do.md";
+import { getPackageState, selectActionableTodayFactory, Task } from "do.md";
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { groupBy, mapToObj } from "remeda";
 import { AppState } from "../../../../store";
 import TaskItem, {
   TaskItemActionSet,
 } from "../../../AllTasks/scenes/TaskItem/TaskItem.scene";
+
+enum TaskGroups {
+  today = "today",
+  overdue = "overdue",
+  undated = "undated",
+}
 
 const ReviewAll = ({
   showAll,
@@ -27,11 +34,47 @@ const ReviewAll = ({
     []
   );
 
+  const today = useSelector(
+    (state: AppState) => getPackageState(state).query.today
+  );
   const tasks = useSelector(actionableTodaySelector);
   const nowIds = useSelector((state: AppState) => state.now.taskIds);
 
   // This is a list of tasksk which have not yet been added to the "now" list
-  const remainingTasksList = tasks.filter((task) => !nowIds.includes(task.id));
+  const remainingTasksList = useMemo(
+    () => tasks.filter((task) => !nowIds.includes(task.id)),
+    [tasks, nowIds]
+  );
+
+  // Group the tasks by date
+  const taskGroups = useMemo(() => {
+    const base = mapToObj(Object.values(TaskGroups), (val): [
+      TaskGroups,
+      Task[]
+    ] => [val, []]);
+
+    const groups = groupBy(remainingTasksList, (task) => {
+      if (typeof task.data.by === "string" && task.data.by === today) {
+        return TaskGroups.today;
+      }
+      if (typeof task.data.snooze === "string") {
+        if (task.data.snooze === today) {
+          return TaskGroups.today;
+        }
+        // If the task has a `snooze` date, and that date is not today, we can
+        // shortcut to say that this task is overdue because the `snooze` date
+        // is always later than the `after` date, and must always be today or in
+        // the past, otherwise the task is omitted from this list.
+        return TaskGroups.overdue;
+      }
+      if (typeof task.data.after === "string" && task.data.after === today) {
+        return TaskGroups.today;
+      }
+      return TaskGroups.undated;
+    });
+
+    return { ...base, ...groups };
+  }, [today, remainingTasksList]);
 
   return (
     <>
@@ -57,8 +100,29 @@ const ReviewAll = ({
         </>
       )}
       <Typography variant="h2">Review</Typography>
+      <Typography variant="h3">Today</Typography>
       <Paper elevation={1} className={classes.paper}>
-        {remainingTasksList.map((task) => (
+        {taskGroups[TaskGroups.today].map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            actionSet={TaskItemActionSet.review}
+          />
+        ))}
+      </Paper>
+      <Typography variant="h3">Overdue</Typography>
+      <Paper elevation={1} className={classes.paper}>
+        {taskGroups[TaskGroups.overdue].map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            actionSet={TaskItemActionSet.review}
+          />
+        ))}
+      </Paper>
+      <Typography variant="h3">Undated</Typography>
+      <Paper elevation={1} className={classes.paper}>
+        {taskGroups[TaskGroups.undated].map((task) => (
           <TaskItem
             key={task.id}
             task={task}
