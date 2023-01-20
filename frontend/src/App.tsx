0769134d7cfa7a5424d05eadb1f243d04f9bffe9
Callum Macdonald <git@callum-macdonald.com>
2023-01-20 06:53:40 +0100

    Remove unused apollo code.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { ApolloProvider } from "react-apollo";
 
-import apollo from "./apollo";
 import store from "./store";
 
 import Routes from "./scenes/Routes/Routes.scene";
@@ -10,9 +8,7 @@ import Routes from "./scenes/Routes/Routes.scene";
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <ApolloProvider client={apollo}>
-        <Routes />
-      </ApolloProvider>
+      <Routes />
     </Provider>
   );
 };
